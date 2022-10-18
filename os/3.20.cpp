2519802dd8ff8acfa9133e5329559718ecda6532
kevin <2558261731@qq.com>
2022-10-18 16:22:03 +0800

    os

--- os/3.20.cpp
+++ os/3.20.cpp
@@ -2,68 +2,44 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h> // read the number of the last error
-#include <fcntl.h> // file descriptor CONSTANT mode values
+#include <fcntl.h>
+#include<wait.h>
 
 int main(int argc, char *argv[])
 {
-
-    /*
-     * File descriptor array.
-     * When we create our pipe, we give pipe() this array and it will
-     * put the reading[0] and writing[1] file descriptors for the pipe into it.
-     */
-
-    int pipeFds[2];
-    int fileBytesLength;
-    char buffer[100];
-    char childBuffer[100];
-
-    // Check if 3 arguments were supplied.
-    if (argc != 3)
+    if (argc!=3)
     {
-        perror("Filecopy: filecopy.exe [target] [destination]. \n");
-        exit(1);
+        printf("error!\n");
     }
-
-    char *srcFile = argv[1];
-    char *dstFile = argv[2];
-
-    // Attempt to create a pipe.
-    if (pipe(pipeFds) < 0)
+    
+    char *input = argv[1];
+    char *copy = argv[2];
+    int fd[2];
+    char buf[100];
+    char childbuf[100];
+    pipe(fd);
+    pid_t pid = fork();
+    if (pid < 0)
     {
-        printf("Something went wrong creating the pipe! %s\n", strerror(errno));
-        exit(1);
+        printf("error!\n");
     }
-
-    // Fork child process
-    switch (fork())
+    else if (pid == 0)
     {
-
-    // If there was an errorforking a child process
-    case -1:
-        printf("Error forking child process. %s\n", strerror(errno));
-        exit(1);
-
-    // If the current executing process is a child process
-    // Read the file from upstream parent process and write it to a new file.
-    case 0:
-        close(pipeFds[1]);                                                            // Close writing end of pipe upstream.
-        ssize_t num_bytes_child = read(pipeFds[0], childBuffer, sizeof(childBuffer)); // Read file contents from upstream pipe into childBuffer
-        close(pipeFds[0]);                                                            // close reading upstream pipe when we're done with it
-
-        int targetDesc = open(dstFile, O_CREAT | O_WRONLY); // Open a file for writing, create file descriptor.
-        write(targetDesc, childBuffer, num_bytes_child);    // Write contents of buffer to new file descriptor.
-
-    // If the current process is the parent process.
-    // Read the file and send it down to the child process to write.
-    default:
-        close(pipeFds[0]);                                            // close reading end of pipe downstream.
-        int fileInDesc = open(srcFile, O_RDONLY);                     // Read file into file descriptor
-        ssize_t num_bytes = read(fileInDesc, buffer, sizeof(buffer)); // Get number bytes to read
-        write(pipeFds[1], buffer, num_bytes);                         // Write bytes to child process.
-        close(pipeFds[1]);                                            // Close writing downstream pipe when we're done with it.
+        close(fd[1]);
+        ssize_t num_bytes_child =read(fd[0],childbuf,sizeof(childbuf));
+        close(fd[0]);
+        int targetDesc = open(copy, O_CREAT | O_WRONLY);
+        write(targetDesc, childbuf, num_bytes_child);
     }
-
+    else
+    {
+        close(fd[0]);
+        int fileInDesc = open(input, O_RDONLY);
+        ssize_t num_bytes = read(fileInDesc, buf, sizeof(buf));
+        write(fd[1],buf,num_bytes);
+        close(fd[1]);
+        wait(NULL);
+    }
+    
     return 0;
 }
